7afdf12d65006663a3c9530ce232502c8af80551
balucloud <111088239+balucloud@users.noreply.github.com>
2022-08-12 18:30:06 +0530

    Delete MyLightningComponentController.js

--- MyLightningComponentController.js
+++
@@ -1,9 +0,0 @@
-({ 
-    doInit : function(component, event, helper) { 
-        var action = component.get("c.getContacts"); 
-        action.setCallback(this, function(response){ 
-            component.set("v.rec", response.getReturnValue()); 
-        }); 
-     $A.enqueueAction(action); 
-    } 
-})
